6f11e87ed1c817251db7bba7d732b0992a3c2dd2
Junseo Yoo <59751754+joonyoo181@users.noreply.github.com>
2023-03-21 11:02:10 -0700

    App.js syntax changes ("" -> '')

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/home";
-import Login from "./pages/login/login";
-import Register from "./pages/register/register";
-import Group from "./pages/group/group";
-import Swipe from "./pages/swipe/swipe";
-import Upload from "./pages/upload/upload";
-import Input from "./components/input/input.js";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+import Login from './pages/login/login';
+import Register from './pages/register/register';
+import Group from './pages/group/group';
+import Swipe from './pages/swipe/swipe';
+import Upload from './pages/upload/upload';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path={"/"} element={<Home />} />
-          <Route exact path={"/login"} element={<Login />} />
-          <Route exact path={"/register"} element={<Register />} />
-          <Route exact path={"/group"} element={<Group />} />
-          <Route exact path={"/swipe"} element={<Swipe />} />
-          <Route exact path={"/upload"} element={<Upload />} />
+          <Route exact path={'/'} element={<Home />} />
+          <Route exact path={'/login'} element={<Login />} />
+          <Route exact path={'/register'} element={<Register />} />
+          <Route exact path={'/group'} element={<Group />} />
+          <Route exact path={'/swipe'} element={<Swipe />} />
+          <Route exact path={'/upload'} element={<Upload />} />
         </Routes>
       </Router>
     </>
