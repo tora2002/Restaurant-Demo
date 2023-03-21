b44296428d1b0d1149be11a39c45001e91c4e95b
Junseo Yoo <59751754+joonyoo181@users.noreply.github.com>
2023-03-21 11:56:02 -0700

    Uppercased Home page in the import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/home';
+import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Group from './pages/Group/Group';
