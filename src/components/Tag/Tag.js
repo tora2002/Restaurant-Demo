69b40022c068e7bcba9d8332872011c92a8dc1bc
Aneri Sheth <anerisheth1@gmail.com>
2023-03-23 17:41:36 -0700

    completed Tag Component!

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './Tag.css'
 
-
 /* Note: Call tag in the following format
     <Tag tagType={'price'} text = {'$20'}/>
     <Tag tagType={'cuisine'} text ={'mexican'}/>
 */
 
-function Tag({tagType, text}) {
-    var temp = tagType.concat('Tag');
+function Tag({tagType = 'default', text}) {
     return(
-        <button className={temp}>
+        <button className= {`tag ${tagType.toLowerCase()}Tag`}>
             {text}
         </button>
-        );
-
-
-    }
+    );
+}
 
 export default Tag;
