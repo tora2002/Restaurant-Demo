ed7b95594cf413193266d4bc934dd47d8005cdee
Junseo Yoo <joon.yoo181@berkeley.edu>
2023-04-03 16:01:27 -0700

    Finished the Waiting Page

--- src/pages/Waiting/Waiting.js
+++ src/pages/Waiting/Waiting.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Waiting.css';
 import Card from '../../components/Card/Card';
 import hero from '../../assets/waiting-hero.gif';
-import UploadBtn from '../../components/UploadButton/UploadButton'
 
 function Waiting() {
 
@@ -10,10 +9,11 @@ function Waiting() {
         //classname 'hero', 'heading' is inherited from home.css. This is a very bad practice!
         <div className='hero'>
             <div className='waiting-card-container'>
-                <UploadBtn/>
                 <Card content={
                     <div className='waiting-card-contents'>
                         <h1 className='waiting-card-heading'>Chill <span>Out</span></h1>
+                        <h3 className='waiting-card-subheading'>Your friends ain't as fast as you boi. While you friends finish matching, go touch some grass</h3>
+                        <img className='waiting-card-hero' src={hero}/>
                     </div>
                 }/>
             </div>
