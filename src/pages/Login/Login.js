05fa5726ccfc0df7f55a69a98cc2d74d41f4cb27
Aneri Sheth <anerisheth1@gmail.com>
2023-04-13 15:16:39 -0700

    login and register page!

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,48 @@
-function Login() {
+import React from 'react';
+import './Login.css';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+import Card from '../../components/Card/Card';
+import Button from '../../components/Button/Button';
+import Input from '../../components/Input/Input';
 
+function Login() {
+    const navigate = useNavigate();
+    return(
+        <>
+        <Header />
+            <div className='mainContainer'>
+                <div className='cardContainer'>
+                    <Card content={
+                        <div className='cardContent'>
+                            <div className='cardText'>
+                                <h1 className='heading'>Taste<span>Buds</span></h1>
+                            </div>
+                            <div className='username'>
+                                <Input placeholder={'username'}
+                                />
+                            </div>
+                            <div className='password'>
+                                <Input placeholder={'password'}
+                                />   
+                            </div>
+
+                            <div className='buttonContainer'>
+                                <Button text={
+                                    <p className='buttonText'>Login</p>
+                                }
+                                />
+                                <Button text={
+                                    <p className='buttonText'>Register</p>
+                                }
+                                onClickHandler={() => navigate("/register")}/>
+                            </div>
+                        </div>
+                    } isFloating={true}/>    
+                    </div>
+            </div>        
+        </>
+    )
 }
 
 export default Login;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,5 +1,58 @@
-function Register() {
+import React from 'react';
+import './Register.css';
+//import { useNavigate } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+import Card from '../../components/Card/Card';
+import Button from '../../components/Button/Button';
+import Input from '../../components/Input/Input';
 
+function Register() {
+    return(
+    <>
+        <Header />
+            <div className='mainContainer'>
+                <div className='cardContainer'>
+                    <Card content={
+                        <div className='cardContent'>
+                            <div className='inputContainer'>
+                                <Input 
+                                        placeholder={'email'}
+                                />
+                                <Input 
+                                    placeholder={'confirm password'}
+                                />
+                            </div>
+
+                            <div className='inputContainer'>
+                                <Input placeholder={'password'}
+                                />
+                                <Input placeholder={'username'}
+                                />
+                            </div>
+                            <div className='inputContainer1'>
+                                <div className='description'>    
+                                    <Input 
+                                            placeholder={'description (250 words max)'}                                           
+                                        />
+                                </div>
+                                <div className='upload'>    
+                                    {/* Need Help Implementing the upload button */}
+                                </div>
+                                
+                            </div>
+                            <div className='buttonContainer'>
+                                <Button text={
+                                    <p className='buttonText'>Register</p>
+                                }
+                                />
+                            </div>
+                        </div>
+                    } isFloating={true}/>    
+                    </div>
+            </div>        
+        </>
+    )
 }
 
 export default Register;
+
