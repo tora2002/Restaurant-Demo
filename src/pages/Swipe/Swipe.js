c986dd8cdcc43dbf61d9e2f44c88de9ae525ad43
Tyler Auton-Smith <tylerautonsmith@gmail.com>
2023-04-14 13:43:12 -0700

    added several pages including a new swipe page. there is also a food component page which needs to be edited and changed so that the card can intake images

--- src/pages/Swipe/Swipe.js
+++ src/pages/Swipe/Swipe.js
@@ -1,5 +1,58 @@
-function Swipe() {
+import React, { useState, useMemo, useRef } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+import Card from '../../components/Card/Card';
+import './Swipe.css'
+import './components/FoodCard/FoodCard.js'
+import FoodCard from './components/FoodCard/FoodCard.js';
 
-}
+  
+export default function Swipe() {
+  const navigate = useNavigate();
 
-export default Swipe;
+  const cards = [
+    {
+      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+      color: '#55ccff'
+    },
+    {
+      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+      color: '#e8e8e8'
+    },
+    {
+      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+      color: '#0a043c'
+    },
+    {
+      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
+      color: 'black'
+    }
+  ];
+
+    return (
+        <>
+          <Header/>
+          <div className='pagey'>
+              <h1 className='heading'>Swipe<span>Ripe</span></h1>
+                  <Card className='beigeCard' content={
+                    <>
+                        <div className='cardyContainer'>
+                          {/* Traversing through cards array using map function
+                            and populating card with different image and color */}
+                          {cards.map((card) => (
+                            <FoodCard image={card.image} color={card.color} />
+                          ))}
+                      </div> 
+                    </>
+                     
+                  }/>
+                   
+              
+        </div>
+        </>
+    )
+  }
+  
+  // ReactDOM.render(<Swipe />, document.getElementById('root'));
+
+
