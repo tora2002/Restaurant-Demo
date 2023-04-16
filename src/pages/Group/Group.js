23394c0877a3f80aa96cc15a5a2462b184305739
Tiffany Li <tiffanyli5511@gmail.com>
2023-04-15 22:12:37 -0700

    fixed class names and moved MemberInfo component into Group page folder

--- src/pages/Group/Group.js
+++ src/pages/Group/Group.js
@@ -5,7 +5,7 @@ import Header from '../../components/Header/Header';
 import Card from '../../components/Card/Card';
 import Button from '../../components/Button/Button';
 import FilterButton from '../../components/FilterButton/FilterButton';
-import MemberInfo from '../../components/MemberInfo/MemberInfo';
+import MemberInfo from './components/MemberInfo/MemberInfo'
 import { Icon } from '@iconify/react';
 
 function Group() {
@@ -27,53 +27,53 @@ function Group() {
     const filters = [
         { 
             type: 'Cuisine',
-            className: 'cuisine',
+            className: 'group-cuisine',
         },
         { 
             type: 'Price Range',
-            className: 'price-range',
+            className: 'group-price-range',
         },
         { 
             type: 'Diet and Allergens',
-            className: 'allergens',
+            className: 'group-allergens',
         },
         { 
             type: 'Meal Type',
-            className: 'meal-type',
+            className: 'group-meal-type',
         },
     ];
 
     const cuisines = [
         { 
             type: 'Mexican',
-            img: 'mexican-food-img',
+            img: 'group-mexican-food-img',
         },
         { 
             type: 'American',
-            img: 'american-food-img',
+            img: 'group-american-food-img',
         },
         { 
             type: 'Chinese',
-            img: 'chinese-food-img',
+            img: 'group-chinese-food-img',
         },
         { 
             type: 'Japanese',
-            img: 'japanese-food-img',
+            img: 'group-japanese-food-img',
         },
         { 
             type: 'Korean',
-            img: 'korean-food-img',
+            img: 'group-korean-food-img',
         },
         { 
             type: 'Indian',
-            img: 'indian-food-img',
+            img: 'group-indian-food-img',
         },
     ];
 
     const priceRanges = [
-        { amount: '&lt; $15' },
+        { amount: '< $15' },
         { amount: '$15 - $25' },
-        { amount: '$25 &gt;' },
+        { amount: '$25 >' },
     ];
 
     const allergens = [
@@ -143,7 +143,7 @@ function Group() {
 
     const groupMembers = () => {
         return (
-            <div className='right-card-content'>
+            <div className='group-right-card-content'>
                 <h2 className='group-member-title'>Group members: </h2>
                 {members.map((member, index) => (
                     <MemberInfo key={index} name={member.name} />
@@ -153,9 +153,9 @@ function Group() {
     };
 
     const cuisineIndividualCards = cuisines.map((cuisine, index) => (
-        <div className='cuisine-individual-card'>
+        <div className='group-cuisine-individual-card'>
             <FilterButton content={
-                <div className='cuisine-content'>
+                <div className='group-cuisine-content'>
                     <p>{cuisine.type}</p>
                     <div className={cuisine.img}></div>
                 </div>
@@ -168,11 +168,11 @@ function Group() {
 
     const cuisineCards = () => {
         return (
-            <div className='cuisine-cards-container'>
-                <div className='cuisine-row-container'>
+            <div className='group-cuisine-cards-container'>
+                <div className='group-cuisine-row-container'>
                     {cuisineIndividualCards.slice(0, cuisineIndividualCards.length/2)}
                 </div>
-                <div className='cuisine-row-container'>
+                <div className='group-cuisine-row-container'>
                     {cuisineIndividualCards.slice(cuisineIndividualCards.length/2)}
                 </div>
             </div>
@@ -181,9 +181,9 @@ function Group() {
 
     const priceRangeCards = () => {
         return (
-            <div className='cuisine-row-container'>
+            <div className='group-cuisine-row-container'>
                 {priceRanges.map((priceRange, index) => (
-                    <div className='small-individual-card'>
+                    <div className='group-small-individual-card'>
                         <FilterButton content={
                             <div>
                                 <p>{priceRange.amount}</p>
@@ -199,7 +199,7 @@ function Group() {
     };
 
     const allergensIndividualCards = allergens.map((allergen, index) => (
-        <div className='cuisine-individual-card'>
+        <div className='group-cuisine-individual-card'>
             <FilterButton content={
                 <div>
                     <p>{allergen.type}</p>
@@ -213,11 +213,11 @@ function Group() {
 
     const allergensCards = () => {
         return (
-            <div className='cuisine-cards-container'>
-                <div className='cuisine-row-container'>
+            <div className='group-cuisine-cards-container'>
+                <div className='group-cuisine-row-container'>
                     {allergensIndividualCards.slice(0, allergensIndividualCards.length/2)}
                 </div>
-                <div className='cuisine-row-container'>
+                <div className='group-cuisine-row-container'>
                     {allergensIndividualCards.slice(allergensIndividualCards.length/2)}
                 </div>
             </div>
@@ -225,7 +225,7 @@ function Group() {
     };
 
     const mealsIndividualCards = meals.map((meal, index) => (
-        <div className='cuisine-individual-card'>
+        <div className='group-cuisine-individual-card'>
             <FilterButton content={
                 <div>
                     <p>{meal.type}</p>
@@ -239,11 +239,11 @@ function Group() {
 
     const mealCards = () => {
         return (
-            <div className='cuisine-cards-container'>
-                <div className='cuisine-row-container'>
+            <div className='group-cuisine-cards-container'>
+                <div className='group-cuisine-row-container'>
                     {mealsIndividualCards.slice(0, mealsIndividualCards.length/2)}
                 </div>
-                <div className='cuisine-row-container'>
+                <div className='group-cuisine-row-container'>
                     {mealsIndividualCards.slice(mealsIndividualCards.length/2)}
                 </div>
             </div>
@@ -256,11 +256,11 @@ function Group() {
             <div className='group-hero'>
                 <h1 className='group-title'>Choose your preferences and create your group</h1>
                 <div className='group-cards'>
-                    <div className='left-card-container'>
+                    <div className='group-left-card-container'>
                         <Card content={
-                            <div className='left-card-content'>
+                            <div className='group-left-card-content'>
                                 <div className='group-filters'>
-                                    <div className='filter'>
+                                    <div className='group-filter'>
                                         <Card content={
                                             <Icon fontSize={30} icon='material-symbols:filter-list-rounded' color='black' />
                                         }
@@ -285,22 +285,22 @@ function Group() {
                             </div>
                         }/>
                     </div>
-                    <div className='right-card-container'>
+                    <div className='group-right-card-container'>
                         <Card content={
                             groupMembers()
                         }/>
                     </div>
                 </div>
                 <div className='group-cards'>
-                    <div className='bottom-card-container'>
+                    <div className='group-bottom-card-container'>
                         <Card content={
-                            <div className='bottom-card-content'>
-                                <button className='generate-link'>Generate Group Link: </button>
-                                <div className='bottom-card-template'></div>
+                            <div className='group-bottom-card-content'>
+                                <button className='group-generate-link'>Generate Group Link: </button>
+                                <div className='group-bottom-card-template'></div>
                             </div>
                         }/>
                     </div>
-                    <div className='bottom-right-button-container'><Button text={
+                    <div className='group-bottom-right-button-container'><Button text={
                         <p>Ready to swipe!</p>
                     }
                     onClickHandler={() => navigate("/swipe")}/>
