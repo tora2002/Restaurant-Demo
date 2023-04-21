9c22082f5d6c59844062218af5d9a2763a797cb6
Tyler Auton-Smith <tylerautonsmith@gmail.com>
2023-04-20 21:07:38 -0700

    working swipe page layout and swipeable components

--- src/pages/Swipe/Swipe.js
+++ src/pages/Swipe/Swipe.js
@@ -6,52 +6,36 @@ import './Swipe.css'
 import './components/FoodCard/FoodCard.js'
 import FoodCard from './components/FoodCard/FoodCard.js';
 
+
+
   
 export default function Swipe() {
   const navigate = useNavigate();
 
-  const cards = [
-    {
-      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
-      color: '#55ccff'
-    },
-    {
-      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
-      color: '#e8e8e8'
-    },
-    {
-      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
-      color: '#0a043c'
-    },
-    {
-      image: 'https://img.icons8.com/color/452/GeeksforGeeks.png',
-      color: 'black'
-    }
-  ];
-
+  const handleSwipe = (d) => {
+    //fill this your callback
+  };
+//This is a failed attempt at mapping the code
+  // const arr = [
+  //   {
+  //     FoodCard
+  //   },
+  //   {
+  //     FoodCard
+  //   }
+  // ]
     return (
         <>
           <Header/>
-          <div className='swipe-heading'>
+          <div className='swipe-page-layout'>
               <h1 className='heading'>Swipe<span>Ripe</span></h1>
-                  <Card className='beigeCard' content={
-                    <>
-                        <div className='cardyContainer'>
-                          
-                          {cards.map((card) => (
-                            <FoodCard image={card.image} color={card.color} />
-                            
-                          ))}
-                      </div> 
-                    </>
-                     
-                  }/>
-                   
-              
+              <Card className='beigeCard' content={
+                <>
+                  <FoodCard/>
+                </>
+              }/>
         </div>
         </>
     )
   }
   
-
-
--- src/pages/Swipe/components/FoodCard/FoodCard.js
+++ src/pages/Swipe/components/FoodCard/FoodCard.js
@@ -1,72 +1,55 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './FoodCard.css';
-import { motion , useMotionValue, useTransform, useAnimation,  } from 'framer-motion';
+import testImage from '../../Bread.jpeg'
+import Tag from '../../../../components/Tag/Tag.js'
+import { CardSwiper } from "react-card-rotate-swiper";
 
-  
 // Card component with destructured props
 function FoodCard({image, menuName, location, description, tags, color}) {
-  // To move the card as the user drags the cursor
-  const motionValue = useMotionValue(0);
-  
-  // To rotate the card as the card moves on drag
-  const rotateValue = useTransform(motionValue, [-200, 200], [-50, 50]);
-  
-  // To decrease opacity of the card when swiped
-  // on dragging card to left(-200) or right(200)
-  // opacity gradually changes to 0
-  // and when the card is in center opacity = 1
-  const opacityValue = useTransform(
-    motionValue,
-    [-200, -150, 0, 150, 200],
-    [0, 1, 1, 1, 0]
-  );
-  
-  // Framer animation hook
-  const animControls = useAnimation();
-  
-  // Some styling for the card
-  // it is placed inside the card component
-  // to make backgroundImage and backgroundColor dynamic
-  const style = {
-    backgroundImage: `url(${image})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
-    backgroundColor: color,
-    boxShadow: '5px 10px 18px #888888',
-    borderRadius: 10,
-    height: 250
+  const handleSwipe = (d) => {
+    //fill this your callback
   };
   
-  return (
-    <div className='cardy'>
-      <motion.div
-        center
-        // Card can be drag only on x-axis
-        drag='x'
-        x={motionValue}
-        rotate={rotateValue}
-        opacity={opacityValue}
-        dragConstraints={{ left: -1000, right: 1000 }}
-        style={style}
-        onDragEnd={(event, info) => {
-          console.log(motionValue, 'info point x')
-          // If the card is dragged only upto 150 on x-axis
-          // bring it back to initial position
-          if (Math.abs(info.point.x) <= 150) {
-            animControls.start({ x: 0 });
-          } else {
-            
-            // If card is dragged beyond 150
-            // make it disappear
   
-            // Making use of ternary operator
-            animControls.start({ x: info.point.x < 0 ? -200 : 200 });
-          }
-        }}
-      />
-    </div>
+  return (
+    <>
+      <CardSwiper
+        onSwipe={handleSwipe}
+        className={"swiper"}
+        contents={
+          <>
+          <div className="swipeCard">
+            <div className='topHalf'>
+              <img src={testImage} className='imageBox'/> 
+              <div className='tagBox'>
+                <Tag tagType={'cuisine'} text = {'Anerr'}> </Tag>
+                <Tag tagType={'price'} text = {'Jermmm'}>yo </Tag>
+                <Tag tagType={'allergen'} text = {'Shammm'}>yo </Tag>
+                <Tag tagType={'meal'} text = {'Tiff'}>yo </Tag>
+              </div>
+                
+            </div>
+            <div className='bottomHalf'>
+            <hr className='' style={{width: '100%', background: 'linear-gradient(to bottom right, #ffcf42, #EC6F34, #E24C52, #C6427B)', height: '40px', border: '0px',}} />
+              <div className='littleDiv'>
+                <h1 className='menuName'>Menu Name</h1>
+                <h3 className='location'>location </h3>
+              </div>
+                <p> Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                  ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                  velit esse
+                </p>
+                <hr className='' style={{width: '100%', background: 'linear-gradient(to bottom right, #ffcf42, #EC6F34, #E24C52, #C6427B)', height: '40px', border: '0px',}} />
+            </div>
+            
+          </div>
+        </>
+         }
+      /> 
+    </>
   );
 };
   
