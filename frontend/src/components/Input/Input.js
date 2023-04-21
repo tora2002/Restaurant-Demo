18ee81348f218e8d2202a013632864571db084a9
Junseo Yoo <joon.yoo181@berkeley.edu>
2023-04-20 21:57:04 -0700

    Added Header.css and Tag.js

--- frontend/src/components/Input/Input.js
+++ frontend/src/components/Input/Input.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Input.css';
 
-function Input( {placeholder, id, value} ) {
+function Input( {className, placeholder, id, value} ) {
   return (
     <input
-      className='custom-input' // add a class name to your custom input
+      className={"custom-input " + className} // add a class name to your custom input
       placeholder={placeholder}
       id={id}
       value={value}
