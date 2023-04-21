56a0fbab37384fbc22f97fe5acfe9f44d86295cd
Junseo Yoo <joon.yoo181@berkeley.edu>
2023-04-20 23:21:44 -0700

    Fixed Swipe page to not change other people's code and implement arr.map

--- frontend/src/pages/Swipe/Swipe.js
+++ frontend/src/pages/Swipe/Swipe.js
@@ -15,27 +15,41 @@ export default function Swipe() {
   const handleSwipe = (d) => {
     //fill this your callback
   };
-//This is a failed attempt at mapping the code
-  // const arr = [
-  //   {
-  //     FoodCard
-  //   },
-  //   {
-  //     FoodCard
-  //   }
-  // ]
-    return (
-        <>
-          <Header/>
-          <div className='swipe-page-layout'>
-              <h1 className='heading'>Swipe<span>Ripe</span></h1>
-              <Card className='beigeCard' content={
+  const arr = [
+    {
+      image: '1',
+      menuName: '2',
+      location: '3',
+      description: '4',
+      tags: '5',
+      color: '6',
+    },
+    {
+      image: '7',
+      menuName: '8',
+      location: '9',
+      description: '10',
+      tags: '11',
+      color: '12',
+    },
+  ]
+
+  return (
+      <>
+        <Header/>
+        <div className='swipe-page-layout'>
+            <h1 className='heading'>Swipe<span>Ripe</span></h1>
+            <div className='swipe-beige-card'>
+              <Card content={
                 <>
-                  <FoodCard/>
+                  {arr.map((element) => (
+                    <FoodCard image={element['image']} menuName={element['menuName']} />
+                  ))}
                 </>
               }/>
-        </div>
-        </>
-    )
-  }
+            </div>
+      </div>
+      </>
+  )
+}
   
